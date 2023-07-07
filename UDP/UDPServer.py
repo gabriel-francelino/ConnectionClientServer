a87b4d928e83d6ee15b386b26fc3261162352482
gabriel-francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-07-07 11:53:05 -0300

    Comando ls funcionando e cd criado

--- UDP/UDPServer.py
+++ UDP/UDPServer.py
@@ -26,15 +26,38 @@ while True:
     # modifiedMessage = 'AOPA BÃO'
 
     # Comando pwd
-    def pwd_command(c):
+    def pwd_command():
         current_dir = os.getcwd()
         serverSocket.sendto(current_dir.encode(), clientAddress)
 
-    command = message.decode().strip().split()[0]
+    # Comando ls
+    def ls_command():
+        file_list = "\n".join(os.listdir())
+        serverSocket.sendto(file_list.encode(), clientAddress)
+
+    # Comando cd
+    def cd_command(*args):
+        new_dir = args[1]
+        print(new_dir)
+
+        if os.path.isdir(new_dir):
+            os.chdir(new_dir)
+            current_dir = pwd_command()
+            serverSocket.sendto(f"Current directory: {current_dir}".encode, clientAddress)
+        else:
+            serverSocket.sendto("Diretório inválido".encode(), clientAddress)
+
+    # Comando scp
+ 
+ 
+    command, *args = message.decode().strip().split()
 
     if command == 'pwd':     
-        pwd_command(command)
-    
+        pwd_command()
+    if command == 'ls':
+        ls_command()
+    if command == 'cd':
+        cd_command(command)
     else:
         serverSocket.sendto("Comando invalido".encode(), clientAddress)
 
